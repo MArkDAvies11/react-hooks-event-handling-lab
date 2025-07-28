70995613d32ecb60f860163e81a6c4739428b366
Mark Davies <markmuiruri11@gmail.com>
2025-07-28 12:26:34 +0300

    done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,23 @@
-// Code Keypad Component Here
+// src/components/Keypad.js
+import React from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() { // This is the ONLY declaration of Keypad
+  const handleChange = (event) => {
+    // You can access the input value via event.target.value if needed later
+    // console.log('Current value:', event.target.value);
+    console.log('Entering password...');
+  };
+
+  return (
+    <div>
+      <label htmlFor="passwordInput">Password:</label>
+      <input
+        id="passwordInput"
+        type="password"
+        onChange={handleChange}
+      />
+    </div>
+  );
 }
 
 export default Keypad;
